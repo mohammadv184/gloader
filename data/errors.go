510be3fcbb415a1db799c267758652e3082fa778
Mohammad Abbasi <mohammad.v184@gmail.com>
2023-01-08 03:57:22 +0330

    chore(type): add assignTo feature to data types

--- data/errors.go
+++ data/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrParseFuncNotImplemented = errors.New("parse function not implemented")
 	ErrDataTypeKindNotMatch    = errors.New("data type kind not match")
 	ErrInvalidValue            = errors.New("invalid passed value")
+	ErrDestMustBePointer       = errors.New("destination must be a pointer")
+	ErrDestNotAssignable       = errors.New("destination is not assignable")
 )
--- data/type.go
+++ data/type.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"errors"
+	"fmt"
 	"reflect"
 	"unsafe"
 )
@@ -12,6 +14,7 @@ type Type interface {
 	GetTypeSize() int
 	GetValue() any
 	To(t Type) (Type, error)
+	AssignTo(t any) error
 }
 
 type BaseType struct{}
@@ -23,7 +26,7 @@ func (_ *BaseType) Parse(_ any) error {
 }
 
 func (_ *BaseType) GetTypeKind() Kind {
-	return GetKindFromName(KindUnknown.String())
+	return KindUnknown
 }
 func (b *BaseType) GetTypeName() string {
 	return reflect.TypeOf(b).String()
@@ -46,4 +49,256 @@ func (b *BaseType) To(t Type) (Type, error) {
 		return nil, err
 	}
 	return t, nil
+}
+func (b *BaseType) AssignTo(dest any) error {
+	if reflect.TypeOf(dest).Kind() != reflect.Ptr {
+		return ErrDestMustBePointer
+	}
+
+	switch b.GetTypeKind() {
+	case KindBool:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = b.GetValue().(bool)
+			return nil
+		case *int:
+			*dest.(*int) = 0
+			if b.GetValue().(bool) {
+				*dest.(*int) = 1
+			}
+			return nil
+		case *int8:
+			*dest.(*int8) = 0
+			if b.GetValue().(bool) {
+				*dest.(*int8) = 1
+			}
+			return nil
+		case *int16:
+			*dest.(*int16) = 0
+			if b.GetValue().(bool) {
+				*dest.(*int16) = 1
+			}
+			return nil
+		case *int32:
+			*dest.(*int32) = 0
+			if b.GetValue().(bool) {
+				*dest.(*int32) = 1
+			}
+			return nil
+		case *int64:
+			*dest.(*int64) = 0
+			if b.GetValue().(bool) {
+				*dest.(*int64) = 1
+			}
+			return nil
+		case *uint:
+			*dest.(*uint) = 0
+			if b.GetValue().(bool) {
+				*dest.(*uint) = 1
+			}
+			return nil
+		case *uint8:
+			*dest.(*uint8) = 0
+			if b.GetValue().(bool) {
+				*dest.(*uint8) = 1
+			}
+			return nil
+		case *uint16:
+			*dest.(*uint16) = 0
+			if b.GetValue().(bool) {
+				*dest.(*uint16) = 1
+			}
+			return nil
+		case *uint32:
+			*dest.(*uint32) = 0
+			if b.GetValue().(bool) {
+				*dest.(*uint32) = 1
+			}
+			return nil
+		case *uint64:
+			*dest.(*uint64) = 0
+			if b.GetValue().(bool) {
+				*dest.(*uint64) = 1
+			}
+			return nil
+		case *float32:
+			*dest.(*float32) = 0
+			if b.GetValue().(bool) {
+				*dest.(*float32) = 1
+			}
+			return nil
+		case *float64:
+			*dest.(*float64) = 0
+			if b.GetValue().(bool) {
+				*dest.(*float64) = 1
+			}
+			return nil
+		case *string:
+			*dest.(*string) = fmt.Sprintf("%v", b.GetValue())
+			return nil
+		default:
+			return fmt.Errorf("from bool to %T: %w", dest, ErrDestNotAssignable)
+		}
+	case KindInt:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = b.GetValue().(int)
+			return nil
+		case *int8:
+			*dest.(*int8) = int8(b.GetValue().(int))
+			return nil
+		case *int16:
+			*dest.(*int16) = int16(b.GetValue().(int))
+			return nil
+		case *int32:
+			*dest.(*int32) = int32(b.GetValue().(int))
+			return nil
+		case *int64:
+			*dest.(*int64) = int64(b.GetValue().(int))
+			return nil
+		default:
+			return fmt.Errorf("from int to %T: %w", dest, ErrDestNotAssignable)
+		}
+
+	case KindInt8:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int8) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = int(b.GetValue().(int8))
+			return nil
+		case *int8:
+			*dest.(*int8) = b.GetValue().(int8)
+			return nil
+		case *int16:
+			*dest.(*int16) = int16(b.GetValue().(int8))
+			return nil
+		case *int32:
+			*dest.(*int32) = int32(b.GetValue().(int8))
+			return nil
+		case *int64:
+			*dest.(*int64) = int64(b.GetValue().(int8))
+			return nil
+		default:
+			return fmt.Errorf("from int8 to %T: %w", dest, ErrDestNotAssignable)
+		}
+
+	case KindInt16:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int16) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = int(b.GetValue().(int16))
+			return nil
+		case *int8:
+			*dest.(*int8) = int8(b.GetValue().(int16))
+			return nil
+		case *int16:
+			*dest.(*int16) = b.GetValue().(int16)
+			return nil
+		case *int32:
+			*dest.(*int32) = int32(b.GetValue().(int16))
+			return nil
+		case *int64:
+			*dest.(*int64) = int64(b.GetValue().(int16))
+			return nil
+		default:
+			return fmt.Errorf("from int16 to %T: %w", dest, ErrDestNotAssignable)
+		}
+
+	case KindInt32:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int32) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = int(b.GetValue().(int32))
+			return nil
+		case *int8:
+			*dest.(*int8) = int8(b.GetValue().(int32))
+			return nil
+		case *int16:
+			*dest.(*int16) = int16(b.GetValue().(int32))
+			return nil
+		case *int32:
+			*dest.(*int32) = b.GetValue().(int32)
+			return nil
+		case *int64:
+			*dest.(*int64) = int64(b.GetValue().(int32))
+			return nil
+		default:
+			return fmt.Errorf("from int32 to %T: %w", dest, ErrDestNotAssignable)
+		}
+
+	case KindInt64:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int64) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = int(b.GetValue().(int64))
+			return nil
+		case *int8:
+			*dest.(*int8) = int8(b.GetValue().(int64))
+			return nil
+		case *int16:
+			*dest.(*int16) = int16(b.GetValue().(int64))
+			return nil
+		case *int32:
+			*dest.(*int32) = int32(b.GetValue().(int64))
+			return nil
+		case *int64:
+			*dest.(*int64) = b.GetValue().(int64)
+			return nil
+		default:
+			return fmt.Errorf("from int64 to %T: %w", dest, ErrDestNotAssignable)
+		}
+	case KindUint:
+		switch dest.(type) {
+		case *bool:
+			*dest.(*bool) = false
+			if b.GetValue().(int) != 0 {
+				*dest.(*bool) = true
+			}
+			return nil
+		case *int:
+			*dest.(*int) = b.GetValue().(int)
+			return nil
+		case *int8:
+			*dest.(*int8) = int8(b.GetValue().(int))
+			return nil
+		case *int16:
+			*dest.(*int16) = int16(b.GetValue().(int))
+			return nil
+		case *int32:
+			*dest.(*int32) = int32(b.GetValue().(int))
+			return nil
+		case *int64:
+			*dest.(*int64) = int64(b.GetValue().(int))
+			return nil
+		}
+		return fmt.Errorf("from int to %T: %w", dest, ErrDestNotAssignable)
+	}
+	//TODO: add more types
+	return errors.New("not implemented")
 }
