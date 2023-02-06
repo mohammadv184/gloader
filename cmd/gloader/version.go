94509066f94939bbba42f805917b936d136e13b2
Mohammad Abbasi <mohammad.v184@gmail.com>
2023-02-06 09:02:23 +0330

    cli(version): add version command

---
+++ cmd/gloader/version.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/spf13/cobra"
+
+// it's populated during build time by -ldflags
+var (
+	version = "dev"
+	commit  = "?"
+	date    = ""
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of gloader",
+	Long: `Print full version information of gloader
+            output format: gloader version <version> <commit> <date>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Println("gloader version", version, commit, date)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
