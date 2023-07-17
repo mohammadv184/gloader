6468ca8a887835a8ddb0d01d4f8bc90df582675d
Mohammad Abbasi <mohammad.v184@gmail.com>
2023-07-17 23:30:45 +0330

    feat(dataMap): add has default value field

--- data/datamap.go
+++ data/datamap.go
@@ -3,23 +3,37 @@ package data
 // Map is a map of data types. It is used to determine schema of dataCollection.
 // The key is the name of the data type and the value is the data type.
 type Map struct {
-	typesMap      map[string]Type
-	isNullableMap map[string]bool
-	typesIndex    []string
+	typesMap        map[string]Type
+	isNullableMap   map[string]bool
+	hasDefaultValue map[string]bool
+	typesIndex      []string
 }
 
 // Get returns the data type with the given name.
 // It returns nil if the data type does not exist.
 func (m *Map) Get(key string) Type {
+	m.init()
 	return m.typesMap[key]
 }
 
 // GetIndex returns the data type with the given index.
 func (m *Map) GetIndex(index int) Type {
+	m.init()
 	return m.typesMap[m.typesIndex[index]]
 }
 
+func (m *Map) GetTypeMap() map[string]Type {
+	m.init()
+	return m.typesMap
+}
+
+func (m *Map) GetNullableMap() map[string]bool {
+	m.init()
+	return m.isNullableMap
+}
+
 func (m *Map) IsNullable(key string) bool {
+	m.init()
 	if _, ok := m.isNullableMap[key]; !ok {
 		return false
 	}
@@ -27,10 +41,27 @@ func (m *Map) IsNullable(key string) bool {
 	return m.isNullableMap[key]
 }
 
+func (m *Map) HasDefaultValue(key string) bool {
+	m.init()
+	if _, ok := m.hasDefaultValue[key]; !ok {
+		return false
+	}
+
+	return m.hasDefaultValue[key]
+}
+
 // Set sets the data type with the given name.
-func (m *Map) Set(key string, value Type, isNullable ...bool) {
-	if len(isNullable) > 0 {
-		m.isNullableMap[key] = isNullable[0]
+// Options[0] isNullable
+// Options[1] hasDefaultValue.
+func (m *Map) Set(key string, value Type, options ...bool) {
+	m.init()
+	for i, v := range options {
+		switch i {
+		case 0:
+			m.isNullableMap[key] = v
+		case 1:
+			m.hasDefaultValue[key] = v
+		}
 	}
 
 	m.typesMap[key] = value
@@ -39,6 +70,7 @@ func (m *Map) Set(key string, value Type, isNullable ...bool) {
 
 // Delete deletes the data type with the given name.
 func (m *Map) Delete(key string) {
+	m.init()
 	for i, k := range m.typesIndex {
 		if k == key {
 			if i == len(m.typesIndex)-1 {
@@ -55,33 +87,71 @@ func (m *Map) Delete(key string) {
 
 // Has returns true if the data type with the given name exists.
 func (m *Map) Has(key string) bool {
+	m.init()
 	_, ok := m.typesMap[key]
 	return ok
 }
 
 func (m *Map) HasIndex(index int) bool {
+	m.init()
 	return index >= 0 && index < len(m.typesIndex)
 }
 
 // Len returns the number of data types in the map.
 func (m *Map) Len() int {
+	m.init()
 	return len(m.typesMap)
 }
 
 // Keys returns the keys of the map.
 func (m *Map) Keys() []string {
+	m.init()
+	return m.typesIndex
+}
+
+func (m *Map) KeysExcept(keys []string) []string {
+	m.init()
+	result := make([]string, 0, len(m.typesIndex))
+	for _, k := range m.typesIndex {
+		for _, key := range keys {
+			if k == key {
+				continue
+			}
+			result = append(result, k)
+		}
+	}
+	return result
+}
+
+func (m *Map) NotNullableKeys() []string {
+	m.init()
 	keys := make([]string, 0, len(m.typesMap))
-	for key := range m.typesMap {
-		keys = append(keys, key)
+	for i, k := range m.typesIndex {
+		if !m.isNullableMap[k] {
+			keys = append(keys, m.typesIndex[i])
+		}
 	}
 	return keys
 }
 
 // Types returns the values of the map.
 func (m *Map) Types() []Type {
+	m.init()
 	types := make([]Type, 0, len(m.typesMap))
 	for _, value := range m.typesMap {
 		types = append(types, value)
 	}
 	return types
+}
+
+func (m *Map) init() {
+	if m.typesMap == nil {
+		m.typesMap = make(map[string]Type)
+	}
+	if m.isNullableMap == nil {
+		m.isNullableMap = make(map[string]bool)
+	}
+	if m.hasDefaultValue == nil {
+		m.hasDefaultValue = make(map[string]bool)
+	}
 }
