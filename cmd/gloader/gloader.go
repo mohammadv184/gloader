ff2720430f2b7cf80038b361c2fafc77a7b768be
Mohammad Abbasi <mohammad.v184@gmail.com>
2023-07-18 20:31:39 +0330

    chore(cli): recover panics

--- cmd/gloader/gloader.go
+++ cmd/gloader/gloader.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gloader command [args] [flags]",
-	Short: "Migrate data from any source to any destination",
+	Use:     "gloader command [args] [flags]",
+	Version: fmt.Sprintf("gloader version %s %s %s", version, commit, date),
+	Short:   "Migrate data from any source to any destination",
 	Long: `Migrate data from any source to any destination in a single command.
            e.g. gloader run mysql://root:root@localhost:3306/tests cockroach://root:root@localhost:5432/tests --filter version<3`,
 }
 
-func Execute() {
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered Err:", r)
+		}
+	}()
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func main() {
-	timeTracker := time.Now()
-	Execute()
-	println(time.Since(timeTracker).String())
 }
