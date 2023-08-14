2b9831df8aa87c618be7614b13e87ab7c0f13ffd
Mohammad Abbasi <mohammad.v184@gmail.com>
2023-08-15 00:06:34 +0330

    ci(build): create completion directory before generating

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,7 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
+  echo "Generating $sh completion..."
+  mkdir "completions/$sh"
 	go run cmd/gloader completion "$sh" >"completions/$sh/goreleaser"
 done
